2b2d1431c0b8b776d3d64a7848cc67c913beb449
Sovereign Framework <sovereign@rawrxd.ai>
2026-08-16 21:55:13 -0400

    chore: gitignore build-ninja artifacts and remove from tracking

--- build-ninja/src/validation/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/rawrxd/src/validation
-# Build directory: D:/rawrxd/build-ninja/src/validation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("kernels")
