f86d85d855018a4bb4b6f08f20753193f6e47a4e
Sovereign Framework <sovereign@rawrxd.ai>
2026-08-24 22:33:06 -0400

    Add PatchRequest and ExecutionBuffer types to OutOfCoreRuntime

--- src/deep2/OutOfCoreRuntime.hpp
+++ src/deep2/OutOfCoreRuntime.hpp
@@ -43,6 +43,14 @@ enum class Backend : uint8_t {
 
 using PatchId = uint64_t;
 
+struct PatchRequest {
+    TensorId tensor = 0;
+    PatchKind kind = PatchKind::Replace;
+    uint64_t offset = 0;
+    const void* data = nullptr;
+    size_t size = 0;
+};
+
 struct TensorDescriptor {
     TensorId id = 0;
     std::string name;
@@ -63,9 +71,19 @@ public:
     bool valid() const { return false; }
 };
 
+class ExecutionBuffer {
+public:
+    virtual ~ExecutionBuffer() = default;
+};
+
 class ExecutionBackend {
 public:
     virtual ~ExecutionBackend() = default;
+    virtual Backend type() const { return Backend::CPU; }
+    virtual std::shared_ptr<ExecutionBuffer> upload(const uint8_t* /*data*/, size_t /*bytes*/) { return nullptr; }
+    virtual bool download(const std::shared_ptr<ExecutionBuffer>& /*buffer*/, uint8_t* /*destination*/, size_t /*bytes*/) { return false; }
+    virtual void release(std::shared_ptr<ExecutionBuffer>& /*buffer*/) {}
+    virtual bool execute(const TensorDescriptor& /*tensor*/, const std::shared_ptr<ExecutionBuffer>& /*buffer*/) { return false; }
 };
 
 class Storage {
@@ -91,6 +109,23 @@ public:
     virtual ~OutOfCoreRuntime() = default;
 
     void setBackend(std::shared_ptr<ExecutionBackend> /*backend*/) {}
+
+    // Stub methods called by SovereignOutOfCoreRuntime
+    void registerTensor(const TensorDescriptor& /*descriptor*/) {}
+    bool hasTensor(TensorId /*id*/) const { return false; }
+    ResidencyLease acquire(TensorId /*id*/, Backend /*backend*/) { return ResidencyLease{}; }
+    bool execute(TensorId /*id*/) { return false; }
+    PatchId installPatch(const PatchRequest& /*request*/) { return 0; }
+    PatchId installPatch(TensorId /*tensor*/, PatchKind /*kind*/, uint64_t /*offset*/, const void* /*data*/, size_t /*size*/) { return 0; }
+    bool removePatch(PatchId /*id*/) { return false; }
+    bool setPatchEnabled(PatchId /*id*/, bool /*enabled*/) { return false; }
+    bool reapplyPatches(TensorId /*id*/) { return false; }
+    Residency residency(TensorId /*id*/) const { return Residency::None; }
+    bool evict(TensorId /*id*/) { return false; }
+    RuntimeStats stats() const { return RuntimeStats{}; }
+    void clearStats() {}
+    size_t ramUsed() const { return 0; }
+    size_t vramUsed() const { return 0; }
 };
 
 } // namespace rawrxd
